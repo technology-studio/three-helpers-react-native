dbf062a222ba99ac982498849162a0bee628a8a5
Erik Slovak <erik.slovak@technologystudio.sk>
2023-04-14 12:17:30 +0200

    feat: manually add types for exports

---
+++ src/index.d.ts
@@ -0,0 +1,4 @@
+export { GLTFLoader } from '../src/types/GLTFLoader'
+export { shaderMaterial } from '../src/types/shaderMaterial'
+export { useGLTF } from '../src/types/useGLTF'
+export { useTexture } from '../src/types/useTexture'
