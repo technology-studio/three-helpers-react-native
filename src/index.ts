ffac9c09215fb7aa231aa82f1d509dda6ed3cf63
Erik Slovák <34162967+erik-slovak@users.noreply.github.com>
2023-09-22 09:45:32 +0200

    feat: add initial implementation (#4)
    * feat: add initial implementation
    
    * feat: manually add types for exports
    
    * feat: create TTF glyph loader and add Text3D types
    
    * chore: add temporary type for troika Text component
    
    * chore: temporarily disable eslint rules while typescript types are missing
    
    * chore: sync boilerplate changes
    
    * chore: update all dependencies

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+export {
+  GLTFLoader,
+} from 'three-stdlib'
+export {
+  shaderMaterial,
+  useGLTF,
+  useTexture,
+  Text3D,
+} from '@react-three/drei/native'
+export {
+  useTTFGlyph,
+} from './useTTFGlyph'
---
+++ src/useTTFGlyph.ts
@@ -0,0 +1,73 @@
+/**
+ * @Author: Erik Slovak <erik.slovak@technologystudio.sk>
+ * @Date: 2023-04-14T18:04:40+02:00
+ * @Copyright: Technology Studio
+**/
+
+import { useLoader } from '@react-three/fiber'
+import {
+  Loader,
+  type LoadingManager,
+} from 'three'
+import {
+  Font,
+} from 'three-stdlib'
+import { Text } from 'troika-three-text'
+
+type FontData = (typeof Font)['prototype']['data']
+
+class TTFGlyphLoader extends Loader {
+  json: FontData | null
+  constructor (manager: LoadingManager | undefined) {
+    super(manager)
+    this.json = null
+  }
+
+  load (
+    character: string,
+    onLoad: (mesh: THREE.Mesh) => void,
+    onProgress?: (event: ProgressEvent) => void,
+    onError?: (event: ErrorEvent) => void,
+  ): void {
+    if (this.json == null) {
+      onError?.(new Error('Missing FontData JSON.') as unknown as ErrorEvent)
+      return
+    }
+    // TODO: remove when https://github.com/protectwise/troika/pull/273 is merged
+    /* eslint-disable @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access */
+    const textMesh = new Text()
+    textMesh.text = character
+    textMesh.font = new Font(this.json)
+    textMesh.fontSize = 10
+    textMesh.depthOffset = 2
+    textMesh.sdfGlyphSize = 0
+    textMesh.sync(() => {
+      onLoad(textMesh)
+    })
+    /* eslint-enable @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access */
+  }
+
+  async loadAsync (): Promise<THREE.Mesh> {
+    // @ts-expect-error -- `loadAsync` exists on Loader
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/return-await, @typescript-eslint/no-unsafe-return
+    return await super.loadAsync()
+  }
+
+  parse (mesh: THREE.Mesh): THREE.Mesh {
+    return mesh
+  }
+
+  setJson (json: FontData): void {
+    this.json = json
+  }
+}
+
+const extensionsFactory = (json: FontData) =>
+  (loader: Loader) => {
+    (loader as TTFGlyphLoader).setJson(json)
+  }
+
+export const useTTFGlyph = (character: string, json: FontData): THREE.Mesh => {
+  const mesh = useLoader(TTFGlyphLoader, character, extensionsFactory(json))
+  return mesh
+}
